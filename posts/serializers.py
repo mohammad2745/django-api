6ffc748a6cf6495eb94d4433d0a9867f123cd912
mohammad2745 <mohammadfcc2745@gmail.com>
2022-10-28 11:49:36 +0600

    SerializerAdded

---
+++ posts/serializers.py
@@ -0,0 +1,13 @@
+from dataclasses import fields
+import imp
+from rest_framework import serializers
+from .models import Post, Vote
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'posted', 'created']
+
+
+        
